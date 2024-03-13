e079e7239806f7357d702f25ad89fda81fd072a2
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2024-03-13 20:51:16 +0200

    add color for loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -7,7 +7,7 @@ export default function Loader() {
       visible={true}
       height="80"
       width="80"
-      color="slateblue"
+      color="#ffa50073"
       radius="9"
       ariaLabel="three-dots-loading"
       wrapperClass={css.loader}
